a5174bcde70d6c62d47a65f9d749bf0a9d57ee49
Freddie Brown <fred@noser.net>
2018-09-07 00:27:00 +0100

    removed some platform specific code

--- lib/Home.dart
+++ lib/Home.dart
@@ -3,36 +3,29 @@ import 'WorkoutList.dart';
 import 'RoutineList.dart';
 import 'ExerciseList.dart';
 import 'Settings.dart';
-import 'dart:io' show Platform;
 
 class Home extends StatelessWidget{
-  final _work = Runes(' \u{1F3CB} ');
-  final _work1 = Runes('\u{1F501}');
-  final _work2 = Runes('\u{1F938}');
 
   @override
-  Widget build(BuildContext context) {
-    if (Platform.isAndroid) {
-      return Scaffold(
-        bottomSheet: Card(
-          margin: EdgeInsets.all(0.0),
-          child: Padding(
-              padding: EdgeInsets.only(top: 8.0, bottom: 16.0),
-              child: TabBar(
-                labelColor: Colors.blue[200],
-                unselectedLabelColor: Colors.black,
-                tabs: [
-                  Tab(child: Text(
-                      "Workouts", style: const TextStyle(fontSize: 17.0))),
-                  Tab(child: Text(
-                      "Routines", style: const TextStyle(fontSize: 18.0))),
-                  Tab(child: Text(
-                      "Exercises", style: const TextStyle(fontSize: 18.0))),
-                ],
-              )
-          ),
-        ),
-        appBar: AppBar(
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      bottomSheet: Card(
+            margin: EdgeInsets.all(0.0),
+            child: Padding(
+              padding: EdgeInsets.only(top: 8.0, bottom: 30.0),
+                child: TabBar(
+                  labelColor: Colors.blue[200],
+                  unselectedLabelColor: Colors.black,
+                  tabs: [
+                    Tab(child: Text("Workouts", style: const TextStyle(fontSize: 17.0))),
+                    Tab(child: Text("Routines" , style: const TextStyle(fontSize: 18.0))),
+                    Tab(child: Text("Exercises" , style: const TextStyle(fontSize: 18.0))),
+                  ],
+                )
+            ),
+      ),
+      appBar: AppBar(
 //        bottom: TabBar(
 //          tabs: [
 //            Tab(text: "Workouts "+String.fromCharCodes(_work)),
@@ -40,77 +33,26 @@ class Home extends StatelessWidget{
 //            Tab(text: "Exercises "+String.fromCharCodes(_work2)),
 //          ],
 //        ),
-          title: Text('GymBook'),
-          actions: <Widget>[ // Add 3 lines from here...
-            new IconButton(icon: Icon(Icons.list),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Settings()),
-                  );
-                }
-            ),
-          ],
-          centerTitle: true,
-        ),
-        body: TabBarView(
-          children: [
-            WorkoutList(),
-            RoutineList(),
-            ExercisesList(),
-          ],
-        ),
-      );
-    }
-    else{
-      return Scaffold(
-        bottomSheet: Card(
-          margin: EdgeInsets.all(0.0),
-          child: Padding(
-              padding: EdgeInsets.only(top: 8.0, bottom: 40.0),
-              child: TabBar(
-                labelColor: Colors.blue[200],
-                unselectedLabelColor: Colors.black,
-                tabs: [
-                  Tab(child: Text(
-                      "Workouts", style: const TextStyle(fontSize: 17.0))),
-                  Tab(child: Text(
-                      "Routines", style: const TextStyle(fontSize: 18.0))),
-                  Tab(child: Text(
-                      "Exercises", style: const TextStyle(fontSize: 18.0))),
-                ],
-              )
+        title: Text('GymBook'),
+        actions: <Widget>[      // Add 3 lines from here...
+          new IconButton(icon: Icon(Icons.list),
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Settings()),
+                );
+              }
           ),
-        ),
-        appBar: AppBar(
-//        bottom: TabBar(
-//          tabs: [
-//            Tab(text: "Workouts "+String.fromCharCodes(_work)),
-//            Tab(text: "Routines "+String.fromCharCodes(_work1)),
-//            Tab(text: "Exercises "+String.fromCharCodes(_work2)),
-//          ],
-//        ),
-          title: Text('GymBook'),
-          actions: <Widget>[ // Add 3 lines from here...
-            new IconButton(icon: Icon(Icons.list),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Settings()),
-                  );
-                }
-            ),
-          ],
-          centerTitle: true,
-        ),
-        body: TabBarView(
-          children: [
-            WorkoutList(),
-            RoutineList(),
-            ExercisesList(),
-          ],
-        ),
-      );
-    }
-    }
+        ],
+        centerTitle: true,
+      ),
+      body: TabBarView(
+        children: [
+          WorkoutList(),
+          RoutineList(),
+          ExercisesList(),
+        ],
+      ),
+    );
   }
+}
